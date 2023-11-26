3d92309339308f84c70b6549e66ab7277752cc39
fabioarilete <fabioarilete@hotmail.com>
2023-11-26 11:16:21 -0300

    feat: first commit

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY > threshold) {
+        setScrolled(true)
+      }else{
+        setScrolled(false)
+      }
+    }
+    return ()=> window.addEventListener('scroll', handleScroll)
+  },[threshold])
+
+  return scrolled;
+}
